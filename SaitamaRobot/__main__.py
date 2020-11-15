c2c1961995028006ac1430b97cb30096fcc6b50d
Jabed02 <74426173+Jabed02@users.noreply.github.com>
2020-11-15 10:12:18 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -53,15 +53,15 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
-You can find my list of available commands with /help.
+I'm The Official Group Managing Bot Of The Dynamic Network! 
+I specialise in managing all types of discussion groups. 
+You can find my list of available commands with /help
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
-the things I can help you with.
+I'm the group manager bot of the Dynamic Network
+You can find all details below.
 
 *Main* commands available:
  • /help: PM's you this message.
@@ -80,10 +80,8 @@ And the following:
 
 SAITAMA_IMG = "https://telegra.ph/file/46e6d9dfcb3eb9eae95d9.jpg"
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
-You can donate to the original writer of the Base code, Paul
-There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+DONATE_STRING = """Heya, glad to hear you want to 
+But we are not accepting any donations for now."""
 
 IMPORTED = {}
 MIGRATEABLE = []
